8f0fb15f821a2a5f89f24868e8898f0d19c32ff0
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-07-07 06:24:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -177,8 +177,8 @@ async def start(client, message):
         return
     if (AUTH_CHANNEL or STAR_MOVIESS_TAMIL or REQ_CHANNEL) and not await is_subscribed(client, message):
         try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-            invite_link2 = await client.create_chat_invite_link(int(STAR_MOVIESS_TAMIL))
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL)),
+            invite_link2 = await client.create_chat_invite_link(int(STAR_MOVIESS_TAMIL)),
 	    invite_link3 = await client.create_chat_invite_link(int(REQ_CHANNEL))
         except ChatAdminRequired:
             logger.error("Mᴀᴋᴇ sᴜʀᴇ Bᴏᴛ ɪs ᴀᴅᴍɪɴ ɪɴ Fᴏʀᴄᴇsᴜʙ ᴄʜᴀɴɴᴇʟ")
