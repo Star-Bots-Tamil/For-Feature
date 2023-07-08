04db32cebeb939726024449cae0f295f91fe5486
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-07-08 10:57:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,40 +54,37 @@ async def is_subscribed(bot, query=None, userid=None):
 
     ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
 
+    userid == None and query != None:
+    
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
     elif query.from_user.id in ADMINS:
         return True
-        
-    if db2().isActive():
-        return True    
-    try:        
-        if userid == None and query != None:
-            user = await db2().get_user(query.from_user.id, int(userid))
-        else:        
-            if user:
-                return True
-            else:
-                return False
 
-        if not AUTH_CHANNEL:
+
+    if db2().isActive():
+        user = await db2().get_user(query.from_user.id, int(userid))
+        if user:
+            return True
+        else:
+            return False
+
+    if not AUTH_CHANNEL:
+        return True    
+    try:
+        if userid == None and query != None:
+            user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+        else:
+            user = await bot.get_chat_member(AUTH_CHANNEL, int(userid))
+    except UserNotParticipant:
+        pass
+    except Exception as e:
+        logger.exception(e)
+    else:
+        if user.status != enums.ChatMemberStatus.BANNED:
             return True
 
-        try:
-            if userid == None and query != None:
-                user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-            else:
-                user = await bot.get_chat_member(AUTH_CHANNEL, int(userid))
-        except UserNotParticipant:
-            return False
-        except Exception as e:
-            logger.exception(e)
-            return False
-        else:
-            if not (user.status == enums.ChatMemberStatus.BANNED):
-                return True
-
-        return False
+    return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
