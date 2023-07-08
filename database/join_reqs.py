ef8897a450b4996f85b7f212df5c9e4c2f3bfeae
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-07-08 19:47:44 +0530

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -22,18 +22,18 @@ class JoinReqs:
 
     async def add_user(self, user_id, first_name, username, date):
         try:
-            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
+            await self.col.insert_one({"_id": (user_id),"user_id": (user_id), "first_name": first_name, "username": username, "date": date})
         except:
             pass
 
     async def get_user(self, user_id):
-        return await self.col.find_one({"user_id": int(user_id)})
+        return await self.col.find_one({"user_id": (user_id)})
 
     async def get_all_users(self):
         return await self.col.find().to_list(None)
 
     async def delete_user(self, user_id):
-        await self.col.delete_one({"user_id": int(user_id)})
+        await self.col.delete_one({"user_id": (user_id)})
 
     async def delete_all_users(self):
         await self.col.delete_many({})
