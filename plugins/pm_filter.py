1c7c13fe857a41d1596d0e86c6f804aea15ac743
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-07-22 18:39:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -457,7 +457,7 @@ async def advantage_spoll_choker(bot, query):
                         buttons = [[ InlineKeyboardButton("🔍 𝚂𝙴𝙰𝚁𝙲𝙷 𝚃𝙾 𝙶𝙾𝙾𝙶𝙻𝙴 🔎", url=f"https://www.google.com/search?q={reply}") ],[ InlineKeyboardButton("× 𝙲𝙻𝙾𝚂𝙴 ×", callback_data="close") ]]
                         spell = await update.reply_text(text=settings["spelltext"].format(query=search, first_name=update.from_user.first_name, last_name=update.from_user.last_name, title=update.chat.title, mention=update.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(buttons))           
                         await asyncio.sleep(60)
-                        await spell.delete()                    
+                    await spell.delete()                    
 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
