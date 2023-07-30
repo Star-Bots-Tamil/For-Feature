f6eaed36ba7fc7e890758860161de4f954ce0c74
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-07-30 16:11:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -450,6 +450,7 @@ async def advantage_spoll_choker(bot, query):
         else:
             reqstr1 = query.from_user.id if query.from_user else 0
             reqstr = await bot.get_users(reqstr1)
+            settings = await get_settings(grp_id)
             if settings["spell_check"]:
                 reply = search.replace(" ", '+')
                 await bot.send_message(chat_id=LOG_CHANNEL, text=(script.NORSLTS.format(reqstr.id, reqstr.mention, movie)))
