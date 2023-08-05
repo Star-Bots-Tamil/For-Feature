7700761b169a6564bb0eb526d99342ee866eea61
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-08-05 09:20:01 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -88,7 +88,7 @@ async def answer(bot, query):
                 reply_markup=reply_markup))
 
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results - {total}"
+        switch_pm_text = f"{emoji.FILE_FOLDER} Your Requested Movies - {total}"
         if string:
             switch_pm_text += f" for {string}"
         try:
@@ -121,31 +121,3 @@ def get_reply_markup(query):
         ]
         ]
     return InlineKeyboardMarkup(buttons)
-
-
-@Client.on_inline_query()
-async def inline(_, query: InlineQuery):
-        string = query.query.lower()
-        if string == "":
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("Ex ; ```@Googletranslateitbot how to use # hi```"),description="{Text} # {language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
-        	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
-        else:
-        	splitit = string.split("#")        	     	
-        try:
-        		cd = splitit[1].lower().replace(" ", "")
-        		text = splitit[0]
-        except:
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("Ex ; ```@Googletranslateitbot how to use # hi```"),description="{Text}#{language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
-        	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
-        	return
-        try:
-        	translator = Translator()
-        	translation = translator.translate(text,dest = cd)
-        except:
-        	return
-        TRTEXT=  [ InlineQueryResultArticle( title=f"Translated from {translation.src} To {translation.dest}", input_message_content=InputTextMessageContent(translation.text),description=f'{translation.text}',thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
-        try:
-        	await query.answer(results=TRTEXT ,  cache_time=2, switch_pm_text="Google Translater",switch_pm_parameter="start")
-        except:
-        	return
-
