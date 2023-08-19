8c80f8e6be6e270ec6d13ff31cd57fce88e74106
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-08-19 09:43:09 +0530

    Update log_cb.py

--- plugins/log_cb.py
+++ plugins/log_cb.py
@@ -35,3 +35,31 @@ async def __(c, m):
         await m.edit_message_reply_markup(
             InlineKeyboardMarkup([[InlineKeyboardButton("Web URL", url=neko_link)]])
 )
+
+def throwbin(x, proxy):
+    headers = { 'Content-Type': 'application/json;charset=UTF-8', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+    payload = {'id': 'null', 'paste': x, 'title': asciigen(10)}
+    response = requests.put("https://api.throwbin.io/v1/store", headers=headers,json=payload,proxies={"http": proxy, "https": proxy},timeout=20)
+    jsoncont = json.loads(response.content)
+    print("https://throwbin.io/{}".format(jsoncont["id"]))
+    with open ("pasted.txt","a+") as handle:
+        handle.write("https://throwbin.io/{}".format(jsoncont["id"])+"\n")
+
+def pastr(x, proxy):
+    headers = { 'Content-Type': 'application/json;charset=UTF-8', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+    payload = {'destruct': "none", 'paste': x, 'syntax': "nohighlight", 'title': asciigen(10)}
+    response = requests.post("https://pastr.io/api/create", headers=headers,json=payload,proxies={"http": proxy, "https": proxy},timeout=20)
+    jsoncont = json.loads(response.content)
+    print("https://pastr.io/{}".format(jsoncont["data"]["url"]))
+    with open ("pasted.txt","a+") as handle:
+        handle.write("https://pastr.io/{}".format(jsoncont["data"]["url"])+"\n")
+
+def hastebin(x, proxy):
+    headers = { 'Content-Type': 'application/json;charset=UTF-8', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+    payload = str(x)
+    response = requests.post("https://hastebin.com/documents", headers=headers,json=payload,proxies={"http": proxy, "https": proxy},timeout=20)
+    jsoncont = json.loads(response.content)
+    print("https://hastebin.com/{}".format(jsoncont["key"]))
+    with open ("pasted.txt","a+") as handle:
+        handle.write("https://hastebin.com/{}".format(jsoncont["key"])+"\n")
+        
