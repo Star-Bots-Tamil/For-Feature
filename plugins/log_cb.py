026d402800fe3f56d511a8e8d8eed7b6ec09df8e
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-08-19 14:09:57 +0530

    Update log_cb.py

--- plugins/log_cb.py
+++ plugins/log_cb.py
@@ -23,19 +23,19 @@ async def __(c, m):
         await m.answer()
     except Exception:
         pass
-        with open("logs/userge.log", "r") as d_f:
-            text = d_f.read()
-        async with aiohttp.ClientSession() as ses:
-            async with ses.post(
-                NEKOBIN_URL + "api/documents", json={"content": text}
-            ) as resp:
-                if resp.status == 201:
-                    response = await resp.json()
-                    key = response["result"]["key"]
-                    file_ext = ".txt"
-                    final_url = NEKOBIN_URL + key + file_ext
-                    final_url_raw = f"{NEKOBIN_URL}raw/{key}{file_ext}"
+    with open("logs/userge.log", "r") as d_f:
+        text = d_f.read()
+    async with aiohttp.ClientSession() as ses:
+        async with ses.post(
+            NEKOBIN_URL + "api/documents", json={"content": text}
+        ) as resp:
+            if resp.status == 201:
+                response = await resp.json()
+                key = response["result"]["key"]
+                file_ext = ".txt"
+                final_url = NEKOBIN_URL + key + file_ext
+                final_url_raw = f"{NEKOBIN_URL}raw/{key}{file_ext}"
         logger.debug(neko_link)
         await m.edit_message_reply_markup(
-            InlineKeyboardMarkup([[InlineKeyboardButton("Web URL", url=neko_link)]])
+            InlineKeyboardMarkup([[InlineKeyboardButton("Web URL", url=final_url)]])
 )
