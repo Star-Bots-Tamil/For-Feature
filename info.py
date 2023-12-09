16c733fb0ef3c3e89a7a2c763e136284d04e4030
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-09 10:44:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,7 +103,7 @@ RENAME_MODE = bool(environ.get("RENAME_MODE"))
 #Add user id of the user in this field those who you want to be Authentic user for file renaming features
 lazy_renamers = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrenamers for lazrenamers in environ.get('LAZY_RENAMERS', '').split()]
 LAZY_RENAMERS = (lazy_renamers + ADMINS) if lazy_renamers else []
-REQ_CHANNEL = int(environ.get('REQ_CHANNEL'))
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL', '-1001831802226'))
 
 # AI
 OPENAI_API = environ.get("OPENAI_API","")
