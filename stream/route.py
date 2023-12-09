cc4cb5bf9e55a5b83d2d06292213e15a081724e1
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-09 09:51:18 +0530

    Update route.py

--- stream/route.py
+++ stream/route.py
@@ -9,7 +9,7 @@ from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
 from stream.bot import multi_clients, work_loads, StarMoviessBot
 from server.exceptions import FIleNotFound, InvalidHash
-from zzint import StartTime, __version__
+from stream.utils. import StartTime, __version__
 from stream.utils.custom_dl import ByteStreamer
 from stream.utils.time_format import get_readable_time
 from stream.utils.render_template import render_page
