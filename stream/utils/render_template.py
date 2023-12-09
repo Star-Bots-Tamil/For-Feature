42b8e190ca43f12792096cf5b450333a50b600bf
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-09 10:10:52 +0530

    Update render_template.py

--- stream/utils/render_template.py
+++ stream/utils/render_template.py
@@ -1,9 +1,9 @@
 import jinja2
 from info import *
-from lazybot import LazyPrincessBot
-from util.human_readable import humanbytes
-from util.file_properties import get_file_ids
-from server.exceptions import InvalidHash
+from stream.bot import StarMoviessBot
+from stream.utils.human_readable import humanbytes
+from stream.utils.file_properties import get_file_ids
+from stream.server.exceptions import InvalidHash
 import urllib.parse
 import logging
 import aiohttp
