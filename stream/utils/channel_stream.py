3e9b9beff56694c4e954fc64d54baa2687c954cd
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-09 10:07:23 +0530

    Update channel_stream.py

--- stream/utils/channel_stream.py
+++ stream/utils/channel_stream.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters, enums
 from pyshorteners import Shortener
+from shortzy import Shortzy
 from info import info
 from database.utils import progress_for_pyrogram, convert, humanbytes
 from hachoir.metadata import extractMetadata
@@ -23,47 +24,52 @@ async def get_channel_shortlink(link):
             data = await response.json()
             return data["shortenedUrl"]
 
-@Client.on_message(
-    filters.channel
-    & (
-        filters.document
-        | filters.video
-    ),
-    group=4,
-)
+async def get_channel_shortlinkk(url):
+    if True:
+        shortzy = Shortzy(URL_SHORTNER_WEBSITE_API, URL_SHORTENR_WEBSITE)
+        try:
+            url = await shortzy.convert(url)
+        except Exception as e:
+            url = await shortzy.get_quick_link(url)
+
+    return url
+
+@Client.on_message(filters.channel & (filters.document | filters.video)  & ~filters.forwarded, group=-1)
 async def channel_receive_handler(bot, broadcast):
-    if int(broadcast.chat.id) in info.BANNED_CHANNELS:
+    if int(broadcast.chat.id) in BANNED_CHANNELS:
         await bot.leave_chat(broadcast.chat.id)
         return
     try:
-        log_msg = await broadcast.forward(chat_id=info.FILES_CHANNEL)
-        file_name = get_media_file_name(broadcast)
-        file_hash = get_hash(log_msg, info.HASH_LENGTH)
-        stream_link = "https://{}/Watch/{}/{}?hash={}".format(info.FQDN, log_msg.id, file_name, file_hash) if info.ON_HEROKU or info.NO_PORT else \
-            "http://{}:{}/Watch/{}/{}?hash={}".format(info.FQDN,
-                                    info.PORT,
-                                    log_msg.id,
-                                    file_name,
-                                    file_hash)
-        shortened_link = await get_channel_shortlink(stream_link)
-
+#        user_id = message.from_user.id
+#        username =  message.from_user.mention
+        channel_name = broadcast.chat.title
+        channel_id = broadcast.chat.id
+        log_msg = await broadcast.forward(
+            chat_id=LOG_CHANNEL,
+        )
+        fileName = get_name(log_msg)
+        filesize = humanbytes(get_media_file_size(log_msg))
+        star_stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        star_download = f"{URL}download/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+        shortened_link = await get_channel_shortlinkk(star_stream)
         await log_msg.reply_text(
-            text=f"<b>Channel Name :- {broadcast.chat.title}\nChannel ID :- <code>{broadcast.chat.id}</code>\nRequest URL :- https://t.me/{(await bot.get_me()).username}?start=Star_Bots_Tamil_{str(log_msg.id)}</b>",
+            text=f"**Link Generated Successfully\nFile Name :- {fileName} \n\nFile Size :- {filesize}\n\nChannel Name :- `{channel_name}`\n\nChannel ID :-** `{channel_id}`",
             quote=True,
-            parse_mode=ParseMode.HTML
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=star_download),  # we download Link
+                                                InlineKeyboardButton('▶Stream online', url=star_stream)]])  # web stream Link
         )
         await bot.edit_message_reply_markup(
             chat_id=broadcast.chat.id,
             message_id=broadcast.id,
-            reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("📥 Fast Download Link", url=shortened_link)]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📥 Fast Download Link", url=shortened_link)]])  # web stream Link
         )
     except FloodWait as w:
         print(f"Sleeping for {str(w.x)}s")
         await asyncio.sleep(w.x)
-        await bot.send_message(chat_id=info.FILES_CHANNEL,
-                             text=f"<b>Got FloodWait of {str(w.x)}s From {broadcast.chat.title}\n\nChannel ID :-</b> <code>{str(broadcast.chat.id)}</code>",
-                             disable_web_page_preview=True, parse_mode=ParseMode.HTML)
+        await bot.send_message(chat_id=FILES_CHANNEL,
+                             text=f"Got Floodwait Of {str(w.x)}s FROM {broadcast.chat.title}\n\n**CHANNEL ID:** `{str(broadcast.chat.id)}`",
+                             disable_web_page_preview=True)
     except Exception as e:
-        await bot.send_message(chat_id=info.FILES_CHANNEL, text=f"<b>#Error_Trackback :-</b> <code>{e}</code>", disable_web_page_preview=True, parse_mode=ParseMode.HTML)
-        print(f"Can't Edit Broadcast Message!\nError :- {e}")
+        await bot.send_message(chat_id=FILES_CHANNEL, text=f"**#ERROR_TRACKEBACK:** `{e}`", disable_web_page_preview=True)
+        print(f"Can't Edit Boardcast Message!\nError:  **Give me edit permission in updates and bin Channel!{e}**")
