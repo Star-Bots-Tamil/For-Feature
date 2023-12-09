323efda400fde8616427420638246a8071c4f2a7
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-09 12:08:41 +0530

    Update render_template.py

--- stream/utils/render_template.py
+++ stream/utils/render_template.py
@@ -24,7 +24,7 @@ async def render_page(id, secure_hash, src=None):
     tag = file_data.mime_type.split("/")[0].strip()
     file_size = humanbytes(file_data.file_size)
     if tag in ["video", "audio"]:
-        template_file = "template/req.html"
+        template_file = "stream/template/req.html"
     else:
         template_file = "template/dl.html"
         async with aiohttp.ClientSession() as s:
