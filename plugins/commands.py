2b3638568d2b46621807cd3ac3cd50d3fbc03c48
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-12-09 15:09:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -139,25 +139,6 @@ ADMIN = int(os.environ.get("ADMIN", "1391556668"))
 
 BATCH_FILES = {}
 
-async def get_channel_shortlink(link):
-    url = 'https://tnshort.net/api'
-    params = {'api': "d03a53149bf186ac74d58ff80d916f7a79ae5745", 'url': link}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-            data = await response.json()
-            return data["shortenedUrl"]
-
-async def get_channel_shortlinkk(url):
-    if True:
-        shortzy = Shortzy(SHORTLINK_API, SHORTLINK_URL)
-        try:
-            url = await shortzy.convert(url)
-        except Exception as e:
-            url = await shortzy.get_quick_link(url)
-
-    return url
-
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
@@ -876,7 +857,7 @@ async def delete(bot, message):
         if result.deleted_count:
             await msg.edit('<b>File 📂 Successfully Deleted</b>')
         else:
-            # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39 
+            # files indexed before  
             # have original file name.
             result = await Media.collection.delete_many({
                 'file_name': media.file_name,
@@ -916,46 +897,6 @@ async def delete_all_index_confirm(bot, message):
     await Media.collection.drop()
     await message.answer('Please Share & Support Us')
     await message.message.edit('<b>Succesfully Deleted All The Indexed Files.</b>')
-
-@Client.on_message(filters.channel & (filters.document | filters.video)  & ~filters.forwarded, group=-1)
-async def channel_receive_handler(bot, broadcast):
-    if int(broadcast.chat.id) in BANNED_CHANNELS:
-        await bot.leave_chat(broadcast.chat.id)
-        return
-    try:
-#        user_id = message.from_user.id
-#        username =  message.from_user.mention
-        channel_name = broadcast.chat.title
-        channel_id = broadcast.chat.id
-        log_msg = await broadcast.forward(
-            chat_id=LOG_CHANNEL,
-        )
-        fileName = get_name(log_msg)
-        filesize = humanbytes(get_media_file_size(log_msg))
-        star_stream = f"{URL}watch/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        star_download = f"{URL}download/{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
-        shortened_link = await get_channel_shortlinkk(star_stream)
-        await log_msg.reply_text(
-            text=f"**⚡ Link Generated Successfully\nFile Name :- {fileName} \n\nFile Size :- {filesize}\n\nChannel Name :- {channel_name}\n\nChannel ID :-** `{channel_id}`",
-            quote=True,
-            disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("web Download", url=star_download),  # we download Link
-                                                InlineKeyboardButton('▶Stream online', url=star_stream)]])  # web stream Link
-        )
-        await bot.edit_message_reply_markup(
-            chat_id=broadcast.chat.id,
-            message_id=broadcast.id,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📥 Fast Download Link", url=shortened_link)]])  # web stream Link
-        )
-    except FloodWait as w:
-        print(f"Sleeping for {str(w.x)}s")
-        await asyncio.sleep(w.x)
-        await bot.send_message(chat_id=FILES_CHANNEL,
-                             text=f"Got Floodwait Of {str(w.x)}s FROM {broadcast.chat.title}\n\n**CHANNEL ID:** `{str(broadcast.chat.id)}`",
-                             disable_web_page_preview=True)
-    except Exception as e:
-        await bot.send_message(chat_id=FILES_CHANNEL, text=f"**#ERROR_TRACKEBACK:** `{e}`", disable_web_page_preview=True)
-        print(f"Can't Edit Boardcast Message!\nError:  **Give me edit permission in updates and bin Channel!{e}**")
 
 @Client.on_message(filters.command('set_template'))
 async def save_template(client, message):
